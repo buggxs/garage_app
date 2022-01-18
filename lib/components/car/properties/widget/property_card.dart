98b6fc0fdc8fbc8330454c4e4c4fa8f2074da43f
AndreSalzmann <andre.salzmann@plentymarkets.com>
2022-01-18 16:56:27 +0100

    ADD modal on car card click

--- lib/components/car/properties/widget/property_card.dart
+++ lib/components/car/properties/widget/property_card.dart
@@ -74,89 +74,183 @@ class PropertyCard extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
-      child: Card(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    topRight: Radius.circular(4.0),
-                    topLeft: Radius.circular(4.0),
-                  ),
-                  color: colorType[type]!['heading']
-              ),
-              width: double.infinity,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 5.0),
-                    child: content[property]!['card_icon'],
-                  ),
-                  Text(
-                    content[property]!['card_heading'],
-                    style: const TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold
+      child: InkWell(
+        onTap: () => showUpdateModal(context, content),
+        child: Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(4.0),
+                      topLeft: Radius.circular(4.0),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              color: colorType[type]!['divider'],
-              child: const SizedBox(
-                height: 3,
+                    color: colorType[type]!['heading']
+                ),
                 width: double.infinity,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 5.0),
+                      child: content[property]!['card_icon'],
+                    ),
+                    Text(
+                      content[property]!['card_heading'],
+                      style: const TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  gradient: colorType[type]!['body']
+              Container(
+                color: colorType[type]!['divider'],
+                child: const SizedBox(
+                  height: 3,
+                  width: double.infinity,
+                ),
               ),
-              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        content[property]!['last_change_text'],
-                        style: const TextStyle(fontWeight: FontWeight.w500),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      const Text("100.00 km /"),
-                      const Text("08.12.2021"),
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        content[property]!['next_change_text'],
-                        style: const TextStyle(fontWeight: FontWeight.w500),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      const Text("160.000 km oder"),
-                      const Text("08.12.2022"),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+              Container(
+                decoration: BoxDecoration(
+                    gradient: colorType[type]!['body']
+                ),
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          content[property]!['last_change_text'],
+                          style: const TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text("100.00 km /"),
+                        const Text("08.12.2021"),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          content[property]!['next_change_text'],
+                          style: const TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text("160.000 km oder"),
+                        const Text("08.12.2022"),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+
+  void showUpdateModal(
+      BuildContext context,
+      Map<String, Map<String, dynamic>> content
+  ) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: BoxDecoration(
+              gradient: colorType[type]!['body']
+          ),
+          height: 350,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 5.0),
+                          child: content[property]!['card_icon'],
+                        ),
+                        Text(
+                          content[property]!['card_heading'],
+                          style: const TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold
+                          ),
+                        )
+                      ],
+                    ),
+                    InkWell(
+                      onTap: () => Navigator.pop(context),
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 5.0),
+                        child: Icon(Icons.close),
+                      ),
+                    ),
+                  ],
+                ),
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const Text('Modal BottomSheet'),
+                      ElevatedButton(
+                        onPressed: () =>
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) => _buildPopupDialog(context),
+                            ),
+                        child: Text("PopUp")
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    );
+  }
+
+  Widget _buildPopupDialog(BuildContext context) {
+    return new AlertDialog(
+      title: const Text('Popup example'),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Hello"),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Close'),
+        ),
+      ],
+    );
+  }
+
 }
