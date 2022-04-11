d1c2ed3f0b65405c24abd264e84ca074b28af8ff
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2022-04-11 21:56:17 +0200

    refactor project structure

--- lib/core/app_service_locator.dart
+++ lib/core/app_service_locator.dart
@@ -4,11 +4,12 @@ import 'package:garage_app/components/common/widgets/modal_service.dart';
 import 'package:garage_app/components/common/widgets/popup_service.dart';
 import 'package:get_it/get_it.dart';
 
-final app = GetIt.instance;
+final GetIt app = GetIt.instance;
 
 void setup() {
-  app.registerSingleton<PopupService>(PopupService());
-  app.registerSingleton<ModalService>(ModalService());
-  app.registerSingleton<LocalCarService>(LocalCarService());
-  app.registerSingleton<LocalDocumentService>(LocalDocumentService());
+  app
+    ..registerLazySingleton<PopupService>(() => PopupService())
+    ..registerLazySingleton<ModalService>(() => ModalService())
+    ..registerFactory<CarService>(() => LocalCarService())
+    ..registerFactory<DocumentService>(() => LocalDocumentService());
 }
