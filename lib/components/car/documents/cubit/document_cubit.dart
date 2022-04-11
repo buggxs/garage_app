f4c363dff1d74b81dabdf4e5970c93199412de92
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2022-04-11 19:06:41 +0200

    ADD documentList

--- lib/components/car/documents/cubit/document_cubit.dart
+++ lib/components/car/documents/cubit/document_cubit.dart
@@ -1,8 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:garage_app/api/car/car_service.dart';
 import 'package:garage_app/api/car/data/car.dart';
 import 'package:garage_app/api/document/data/document.dart';
 import 'package:garage_app/components/car/cubit/car_cubit.dart';
+import 'package:garage_app/core/app_service_locator.dart';
+import 'package:logging/logging.dart';
 import 'package:meta/meta.dart';
 
 part 'document_state.dart';
@@ -22,14 +25,26 @@ class DocumentCubit extends Cubit<DocumentState> {
     });
   }
 
+  final Logger _log = Logger('DocumentCubit');
+
   CarCubit carCubit;
   Car? car;
 
-  void loadDocuments() {
+  void loadDocuments() async {
     emit(DocumentLoadingState());
 
-    emit(DocumentLoadedState(
-      documentList: car?.documentList ?? [[]],
-    ));
+    if (car == null) {
+      emit(DocumentErrorState(message: 'Fehler beim laden des Auto'));
+      return;
+    }
+
+    Car _car = await app<LocalCarService>().getCarById(carId: car!.id);
+
+    List<List<Document>> documents =
+        _car.documentList ?? <List<Document>>[[], [], []];
+
+    _log.info('Loaded documents of car: ${_car.name}');
+
+    emit(DocumentLoadedState(documentList: documents));
   }
 }
--- lib/components/car/notes/cubit/note_cubit.dart
+++ lib/components/car/notes/cubit/note_cubit.dart
@@ -4,6 +4,7 @@ import 'package:garage_app/api/car/data/car.dart';
 import 'package:garage_app/api/note/data/note.dart';
 import 'package:garage_app/components/car/cubit/car_cubit.dart';
 import 'package:garage_app/core/app_service_locator.dart';
+import 'package:logging/logging.dart';
 import 'package:meta/meta.dart';
 
 part 'note_state.dart';
@@ -20,6 +21,8 @@ class NoteCubit extends Cubit<NoteState> {
     });
   }
 
+  final Logger _log = Logger('NoteCubit');
+
   CarCubit carCubit;
   Car? car;
 
@@ -27,12 +30,15 @@ class NoteCubit extends Cubit<NoteState> {
     emit(NoteLoading());
 
     if (car == null) {
+      _log.warning('Car is null');
       emit(NoteError(message: "Car konnte nicht geladen werden"));
     }
 
     Car _car = await app<LocalCarService>().getCarById(carId: car!.id);
 
     List<Note> carNoteList = _car.noteList ?? <Note>[];
+
+    _log.info('Loaded ${carNoteList.length} notes');
 
     carNoteList.sort((Note a, Note b) => b.dateTime!.compareTo(a.dateTime!));
     emit(NoteLoaded(noteList: carNoteList));
